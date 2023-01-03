8b31017ba921255dd214ff31ac369ab73b199980
Lopettelavrai <121894018+Lopettelavrai@users.noreply.github.com>
2023-01-03 19:23:27 +0100

    Add files via upload

---
+++ ind2.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+	<title>Enseignement de sciences numériques et technologie</title>
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="ind2.css">
+	<link rel="shortcut icon" href="https://ministage.lycee-dauphine.fr/images/dauphine.png" type="image/x-icon">
+	</head> 
+
+<body>
+
+	 <div id="container">
+		<!-- 	<div id="section1">		 
+			</div> --> 
+			<div id="square">
+<a href="index.html"><img alt="SNT" class="center" src="snt.png">	</a>
+		 				
+			</div>
+			
+				<div id="square2">
+
+<img width="150" src="https://ministage.lycee-dauphine.fr/images/dauphine.png">
+		 				
+			</div>
+			
+		</div>
+
+  </body></html>
